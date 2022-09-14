615e732827db0da33f80aaccaeb580e2a7e833fd
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-09-14 10:28:24 -0400

    fixed some problems

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,19 @@
   </div>
 </template>
 
+<script>
+  import Navbar from "./components/Navbar.vue";
+  import CountrierList from "./components/CountrierList.vue";
+  import CountryDetails from "./components/CountryDetails.vue";
+  
+  export default{
+    name: "App",
+    components: {Navbar, CountrierList, CountryDetails},
+  }
+  
+</script>
+
+
 <style>
 body {
   margin: 0;
---
+++ src/components/CountrierList.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+    export default{
+        name: "CountrierList",
+    }
+</script>
+
+<style></style>
